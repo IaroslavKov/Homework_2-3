0be10333c3b45cb4973200a3b4d6bdd151e8d2e5
IaroslavKov <disla.kov@gmail.com>
2022-07-28 19:42:50 +0300

    Txt files like ISBN and Card is for task1 and task2. Also Subnets.txt is task3

---
+++ Lesson10/route_delete.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo route del -net 192.56.76.0 netmask 255.255.255.192 dev eth0
+sudo route del -net 192.56.76.64 netmask 255.255.255.192 dev eth0
+sudo route del -net 192.56.76.128 netmask 255.255.255.192 dev eth0
+sudo route del -net 192.56.76.192 netmask 255.255.255.192 dev eth0
+
